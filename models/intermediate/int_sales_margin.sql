d564a60a47ae07349713045ec3e3c1bc1a95761f
Matteo Cherief <matteo.cherief@orange.fr>
2024-10-29 14:36:47 +0000

    jointure int_sales_margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,12 @@
+SELECT
+date_date
+orders_id,
+pdt_id,
+quantity,
+revenue,
+quantity * purchase_price AS purchase_cost,
+ROUND(sales.revenue - (sales.quantity * product.purchase_price),2) AS margin
+FROM {{ ref('stg_raw__sales') }} AS sales
+LEFT JOIN {{ ref('stg_raw__product') }} AS product
+ON sales.pdt_id = product.products_id
+ORDER BY orders_id
