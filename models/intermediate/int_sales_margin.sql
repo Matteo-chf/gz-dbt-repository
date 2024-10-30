e14e12dea33e0abd0d1d922366a4fcfd2d6edd0f
Matteo Cherief <matteo.cherief@orange.fr>
2024-10-30 15:36:17 +0000

    macros function

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,17 @@
-SELECT
-orders_id,
-date_date,
-pdt_id,
-quantity,
-revenue,
-quantity * purchase_price AS purchase_cost,
-ROUND(sales.revenue - (sales.quantity * product.purchase_price),2) AS margin
-FROM {{ ref('stg_raw__sales') }} AS sales
-LEFT JOIN {{ ref('stg_raw__product') }} AS product
-ON sales.pdt_id = product.products_id
-ORDER BY orders_id
+  WITH sub AS (
+  SELECT
+    sales.date_date,
+    sales.orders_id,
+    sales.pdt_id,
+    sales.quantity,
+    sales.revenue,
+    product.purchase_price,
+    ROUND(purchase_price*sales.quantity,2) AS purchase_cost
+    FROM {{ ref('stg_raw__sales') }} AS sales
+    LEFT JOIN {{ ref('stg_raw__product') }} AS product
+  ON sales.pdt_id = product.products_id
+  )
+ SELECT *,
+    {{ margin_percent('revenue', 'purchase_cost') }} AS margin_percent
+ FROM 
+    sub
