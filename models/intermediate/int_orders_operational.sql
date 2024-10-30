9b0fe26ed66338f2c2dbe43392e782564d1090ce
Matteo Cherief <matteo.cherief@orange.fr>
2024-10-30 10:15:00 +0000

    orders operational modif

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,13 +1,10 @@
 SELECT
-sales_margin.orders_id,
-date_date,
-ROUND((margin + shipping_fee - logcost - ship_cost),2) AS operational_margin,
-ROUND((revenue),2) AS sum_revenue,
-ROUND((quantity),2) AS sum_quantity,
-ROUND((purchase_cost),2) AS purchase_cost,
-ROUND((margin),2) AS margin
-FROM {{ ref('int_sales_margin') }} AS sales_margin
-LEFT JOIN {{ ref('stg_raw__ship') }} AS ship 
-ON sales_margin.orders_id = ship.orders_id
-ORDER BY sales_margin.orders_id DESC
-GROUP BY sales_margin.orders_id
+marg.*,
+ROUND((marg.margin + ship.shipping_fee) - ship.logcost - ship.ship_cost,2) AS operational_margin,
+shipping_fee,
+logcost,
+ship_cost
+FROM {{ ref('int_orders_margin') }} AS marg
+LEFT JOIN {{ ref('stg_raw__ship') }} AS ship
+USING (orders_id)
+ORDER BY orders_id DESC
